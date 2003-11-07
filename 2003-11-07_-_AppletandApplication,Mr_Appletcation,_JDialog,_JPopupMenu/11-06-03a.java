7a8313818672de64f6243035174867c490286636
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2003-11-07 13:59:05 -0500

    Draw, JDialog

--- 2003-11-07_-_AppletandApplication,Mr_Appletcation,_JDialog,_JPopupMenu/11-06-03a.java
+++ 2003-11-07_-_AppletandApplication,Mr_Appletcation,_JDialog,_JPopupMenu/11-06-03a.java
@@ -3,37 +3,33 @@
 //Make this program work both as an applicationa and as an applet
 
 
-public class PrintApplication extends Applet implements ActionListener{
-	JButton b1,b2;
-	public static void main(String[] args){
-		b1=new JButton("PrintAll");
-		b2=new JButton("PrintText");
-		JFrame frameForPrint = new JFrame("Print Demo");
-		frameForPrint.setSize(200,300);
-		frameForPrint.getContentPane().add(b1,BorderLayout.NORTH);
-		frameForPrint.getContentPane(b2, BorderLayout.NORTH);
-		frameForPrint.show();
-		PrintJob printer=this.getToolKit().getPrintJob(frameForPrint,"Printing Page..", null);
-		if(printer!=null)
-			Graphics g=printer.getGraphics();
-		
-	}
-	
-	//constructor if needed
-	
-	public void actionPerformed(ActionEvent e){
-		if(e.getSource()==b1){
-			this.printAll(g);
-			g.dispose();//send the printJob to the printer queue
-			g.end();//ending printjob
-		}
-		else {
-			g.drawString("Send this to the printer");
-			g.dispose();
-			g.end();
-		}
-	}
-		
-		
-			
-		
+public class PrintApplication extends Applet implements ActionListener {
+    JButton b1, b2;
+
+    public static void main(String[] args) {
+        b1 = new JButton("PrintAll");
+        b2 = new JButton("PrintText");
+        JFrame frameForPrint = new JFrame("Print Demo");
+        frameForPrint.setSize(200, 300);
+        frameForPrint.getContentPane().add(b1, BorderLayout.NORTH);
+        frameForPrint.getContentPane(b2, BorderLayout.NORTH);
+        frameForPrint.show();
+        PrintJob printer = this.getToolKit().getPrintJob(frameForPrint, "Printing Page..", null);
+        if (printer != null)
+            Graphics g = printer.getGraphics();
+
+    }
+
+    //constructor if needed
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+            this.printAll(g);
+            g.dispose();//send the printJob to the printer queue
+            g.end();//ending printjob
+        } else {
+            g.drawString("Send this to the printer");
+            g.dispose();
+            g.end();
+        }
+    }
--- 2003-11-07_-_AppletandApplication,Mr_Appletcation,_JDialog,_JPopupMenu/11-06-03b.java
+++ 2003-11-07_-_AppletandApplication,Mr_Appletcation,_JDialog,_JPopupMenu/11-06-03b.java
@@ -1,38 +1,33 @@
-public class ApplOrApplet extends Applet implements ActionLIstener{
-	static boolean appplet=true;
-	
-	public void init(){
-		
-		//call a function addComponents() to add components 
-		//and to add listeners
-	}
-	
-	public static void main(String[] args){
-		JFrame frame=new JFrame("Applet OR Application");
-		
-		//call function addComponents() to add components
-		//and to add listeners
-		appplet=false;
-	}
-	
-	public void addComponents(){
-		//create components here
-		//add listeners here
-	}
-	
-	public void actionPerformed(ActionEvent e){
-		//handle events here
-		if(e.getSource()==...)
-			//do something
-		else //do some other thing
-		
-		if(appplet==true)
-			this.showStatus("Color of Label is " + color);
-	}
-	
+public class ApplOrApplet extends Applet implements ActionLIstener {
+    static boolean appplet = true;
+
+    public void init() {
+
+        //call a function addComponents() to add components
+        //and to add listeners
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Applet OR Application");
+
+        //call function addComponents() to add components
+        //and to add listeners
+        appplet = false;
+    }
+
+    public void addComponents() {
+        //create components here
+        //add listeners here
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        //handle events here
+        if (e.getSource() ==...)
+        //do something
+        else //do some other thing
+
+        if (appplet == true)
+            this.showStatus("Color of Label is " + color);
+    }
+
 }
-
-
-
-
-	
